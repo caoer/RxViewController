fe636e86230cb8bf458b3ee6df237f1cc912701d
Zitao Xiong <caoer115@gmail.com>
2018-10-25 20:57:38 +0800

    carthage support

---
+++ RxViewController/RxViewController.h
@@ -0,0 +1,19 @@
+//
+//  RxViewController.h
+//  RxViewController
+//
+//  Created by Zitao Xiong on 10/25/18.
+//  Copyright © 2018 Zitao. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RxViewController.
+FOUNDATION_EXPORT double RxViewControllerVersionNumber;
+
+//! Project version string for RxViewController.
+FOUNDATION_EXPORT const unsigned char RxViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxViewController/PublicHeader.h>
+
+
